267b6bc02ea584619c2d666ee0c299bbb3c0fa94
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-09-30 21:29:23 +0800

    Initial Commit

---
+++ NavigationControllerDelegateDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationControllerDelegateDemo
+//
+//  Created by kidnapper on 2019/9/30.
+//  Copyright © 2019 kidnapper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
